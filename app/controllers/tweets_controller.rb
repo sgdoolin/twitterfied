27216a64c7ecbe015de9308b078e64e316c2b055
Sean Doolin <sgdoolin@gmail.com>
2016-05-16 19:31:16 -0500

    Set up Create new tweet and show it

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+
   def index
   end
 
@@ -9,7 +12,22 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
+
+def create
+  @tweet = Tweet.new(tweet_params)
+
+  respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+      else
+        format.html { render :new }
+      end
+  end
+end
+
+
 
   def _forms
   end
